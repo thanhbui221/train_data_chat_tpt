9be19744752ce33f337937dbc80d5387182b2f1d
thanhbui221 <thanhfbuif22198@gmail.com>
2023-03-16 22:58:37 +0700

    d

--- venv/lib/python3.10/site-packages/gpt_index/indices/struct_store/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Structured store indices."""
-
-from gpt_index.indices.struct_store.sql import GPTSQLStructStoreIndex
-
-__all__ = ["GPTSQLStructStoreIndex"]
--- venv/lib/python3.10/site-packages/tests/indices/list/__init__.py
+++
@@ -1,7 +0,0 @@
-"""List-based data structures."""
-
-from gpt_index.indices.list.base import GPTListIndex
-
-__all__ = [
-    "GPTListIndex",
-]
